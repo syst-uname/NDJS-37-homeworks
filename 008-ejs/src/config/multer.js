84638673b4954831723ef8dc796fd840eefcb005
Александр <alexxxakm@mail.ru>
2024-07-17 23:33:41 +0300

    008-ejs: правки по замечаниям к ДЗ 007-middleware

--- 008-ejs/src/config/multer.js
+++ 008-ejs/src/config/multer.js
@@ -2,7 +2,7 @@ import multer from "multer"
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, 'storage/public/')
   },
   filename: (req, file, cb) => {
     const fileName = `${req.body.authors} - ${file.originalname}`
