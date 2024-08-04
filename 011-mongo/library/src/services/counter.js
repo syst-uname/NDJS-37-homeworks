bdde903ae075bc0b807a07a76b2f3514bd7ea290
Александр <alexxxakm@mail.ru>
2024-08-05 01:37:09 +0300

    011-mongo: перемещение и переименовывание файла counter.js (для отслеживания изменений)

--- 011-mongo/library/src/services/counter.js
+++ 011-mongo/library/src/services/counter.js
@@ -4,7 +4,7 @@ import config from '../config/index.js';
 class Counter {
   async incr(bookId) {
     try {
-      await axios.post(`${config.server.counter_url}/counter/${bookId}/incr`);
+      await axios.post(`${config.counter.url}/counter/${bookId}/incr`);
     } catch (error) {
       console.error(error);
       throw new Error(`counter error: ${error}`);
@@ -13,7 +13,7 @@ class Counter {
 
   async get(bookId) {
     try {
-      const response = await axios.get(`${config.server.counter_url}/counter/${bookId}`);
+      const response = await axios.get(`${config.counter.url}/counter/${bookId}`);
       return response.data.count
     } catch (error) {
       console.error(error);
