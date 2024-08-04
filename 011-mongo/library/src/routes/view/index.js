a472ab174a3363a51d0b9606a6a4aa917cdd7e0e
Александр <alexxxakm@mail.ru>
2024-08-05 01:54:41 +0300

    011-mongo: переименование файла book.route.js (для отслеживания изменений)

--- 011-mongo/library/src/routes/view/index.js
+++ 011-mongo/library/src/routes/view/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { default as homeRoute } from './home.route.js'
-import { default as booksRoute } from './books.route.js'
+import { default as booksRoute } from './book.route.js'
 import { default as errorRoute } from './error.route.js'
 
 const router = Router()
