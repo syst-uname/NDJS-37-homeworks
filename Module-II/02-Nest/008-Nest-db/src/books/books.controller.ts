b89d14e4f05e5eb7b5533351aca9020385bc33c3
Александр <alexxxakm@mail.ru>
2024-10-14 23:31:26 +0300

    008-nest-db: копирование проекта из 006-nest-ext

---
+++ Module-II/02-Nest/008-Nest-db/src/books/books.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { BooksService } from './books.service'
+import { CreateBookDto, UpdateBookDto } from './dto/book.dto'
+
+@Controller('books')
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.create(createBookDto)
+  }
+
+  @Get()
+  findAll() {
+    return this.booksService.findAll()
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.booksService.findOne(+id)
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateBookDto: UpdateBookDto
+  ) {
+    return this.booksService.update(+id, updateBookDto)
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.booksService.remove(+id)
+  }
+}
---
+++ Module-II/02-Nest/008-Nest-db/src/books/interfaces/book.interface.ts
@@ -0,0 +1,6 @@
+export interface IBook {
+  id: number
+  title: string
+  authors: string
+  description: string
+}
