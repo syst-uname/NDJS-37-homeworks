9f4cd63f2f5b00fc780c6d57d4cd909f5dd760bf
Александр <alexxxakm@mail.ru>
2024-10-23 22:32:36 +0300

    010-validation: копирование компонента Book из проекта 006-Nest-ext

---
+++ Module-II/02-Nest/010-validation/src/book/book.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { BookService } from './book.service'
+import { CreateBookDto, UpdateBookDto } from './dto/book.dto'
+
+@Controller('book')
+export class BookController {
+  constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.bookService.create(createBookDto)
+  }
+
+  @Get()
+  findAll() {
+    return this.bookService.findAll()
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.bookService.findOne(+id)
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateBookDto: UpdateBookDto
+  ) {
+    return this.bookService.update(+id, updateBookDto)
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.bookService.remove(+id)
+  }
+}
